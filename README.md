96964a52372b205607267234d6d68adeca78cfd0
gapfinderindonesia <gapfinderindonesia@gmail.com>
2021-01-25 12:03:35 +0800

    pertama

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# BOT-GFI-V9
+<p align="center">
+<img src="https://static.wikia.nocookie.net/kenja-no-mago/images/8/85/Sizilien_von_klode_1.jpg/revision/latest/top-crop/width/300/height/300?cb=20190417164406" width="128" height="128"/>
+</p>
+<p align="center">
+<a href="#"><img title="BOT GFI" src="https://img.shields.io/badge/BOT GFI-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<a href="https://github.com/XP-TN"><img title="Author" src="https://img.shields.io/badge/Author-XP-TN-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/gapfinderindonesia/followers"><img title="Followers" src="https://img.shields.io/github/followers/XP-TN?color=blue&style=flat-square"></a>
+<a href="https://github.com/gapfinderindonesia/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/XP-TN/XP-TNNBOT?color=red&style=flat-square"></a>
+<a href="https://github.com/gapfinderindonesia/network/members"><img title="Forks" src="http://img.shields.io/github/forks/XP-TN/XP-TNNBOT?color=red&style=flat-square"></a>
+<a href="https://github.com/gapfinderindonesia/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/XP-TN/XP-TNNBOT?label=Watchers&color=blue&style=flat-square"></a>
+<a href="https://hits.seeyoufarm.com"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FXP-TN%2FXP-GFI BOT&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=Support&edge_flat=false"/></a>
+<a href="#"><img title="MAINTENED" src="https://img.shields.io/badge/MAINTENED-YES-blue.svg"</a>
+</p>
+
+## Clone this project
+
+```bash
+> git clone https://github.com/gapfinderindonesia/GFI-BOT-V8
+```
+
+```bash
+>Command BOT $
+```
+
+## Install the dependencies:
+Before running the below command, make sure you're in the project directory that
+you've just cloned!!
+
+```bash
+> cd GFI-BOT-V8
+> bash install.sh
+```
+
+### Usage
+```bash
+> npm start
+```
+
+## Features
+
+| Sticker Creator |                Feature           |
+| :-----------: | :--------------------------------: |
+|       ✅       | Send Photo with Caption          |
+|       ✅       | Reply A Photo                    |
+|       ✅       | Reply A Video or GIF             |
+|       ✅       | Send Video or GIF with Caption   |
+|       ✅       | Reply A Sticker ( sticker to image ) |
+
+| Other  |                     Feature                     |
+| :------------: | :---------------------------------------------: |
+|       ✅        |   Get a random meme             |
+|       ✅        |   Text to speech                |
+|       ✅        |   Writing feature 				|
+|       ✅        |   What Anime Is This 			|
+|       ✅        |   Url2Img ( Screeenshot Web )   |
+|       ✅        |   Simsimi		                |
+
+| Group  |                     Feature               |
+| :-----------: | :--------------------------------: |
+|       ✅        |   Tagall/Mentionall member       |
+|       ✅        |   Kick Member Group	             |
+|       ✅        |   Add Member Group	             |
+|       ✅        |   Get List Admins Group          |
+
+| Owner Bot  |                     Feature           |
+| :-----------: | :--------------------------------: |
+|       ✅        |   Set Prefix                     |
+|       ✅        |   Broadcast                      |
+|       ✅        |   Clear All Chats                |
+
+## Special Thanks to
+* [`Dandi Saputra`]https://www.instagram.com/d_c_d_j
+* [`Dandi Saputra`](wa.me/+62-813-5244-5558)
+
+
+## Donate
+* [`GOJEK`](081352445558)
+### Donate
+* [`Saweria`](https://saweria.co/DandiSaputra)
---
+++ src/help.js
@@ -0,0 +1,250 @@
+const help = (prefix) => { 
+	return `   *🍁MENU BOT G.F.I🍁*
+	
+	
+┏━━━━━━━━━━━━━━━━━━━━━┓
+┃ *⚠️ NO SPAM!! NO CALL!!! ⚠️*
+┃ *LANGGAR BLOCK/BANNED!!!*
+┣━━━━°❀ *❬ ABOUT ❭* ❀°━━━━━━┛
+┃
+┃⊱❥ *BOT GAP FINDER INDONESIA*
+┃⊱❥ *V 6.0*
+┃⊱❥ *http://wa.me/6281352445558*
+┃⊱❥ *Dandi Saputra*             
+┃
+┣━━°❀ ❬ *TENTANG BOT* ❭ ❀°━━┓
+┃
+┣⊱❥ *${prefix}info*
+┣⊱❥ *${prefix}donasi*
+┣⊱❥ *${prefix}owner*
+┣⊱❥ *${prefix}speed*
+┣⊱❥ *${prefix}report [lapor bug]*
+┃͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏ ͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏.
+┣━━━━°❀ ❬ *MEDIA* ❭ ❀°━━━━━┓
+┃
+┣⊱❥ *${prefix}stiker*
+┣⊱❥ *${prefix}tsticker*
+┣⊱❥ *${prefix}toimg*
+┣⊱❥ *${prefix}nulis*
+┣⊱❥ *${prefix}ocr*
+┣⊱❥ *${prefix}ytsearch*
+┣⊱❥ *${prefix}ytmp3*
+┣⊱❥ *${prefix}ytmp4*
+┣⊱❥ *${prefix}tiktok*
+┣⊱❥ *${prefix}tiktokstalk*
+┣⊱❥ *${prefix}fototiktok*
+┣⊱❥ *${prefix}igstalk*
+┣⊱❥ *${prefix}image*
+┣⊱❥ *${prefix}pinterest*
+┣⊱❥ *${prefix}tts*
+┣⊱❥ *${prefix}tes*
+┣⊱❥ *${prefix}tep*
+┣⊱❥ *${prefix}ttp*
+┣⊱❥ *${prefix}meme*
+┣⊱❥ *${prefix}memeindo*
+┣⊱❥ *${prefix}ssweb*
+┣⊱❥ *${prefix}walpaperhd*
+┣⊱❥ *${prefix}randomcat*
+┣⊱❥ *${prefix}joox*
+┣⊱❥ *${prefix}inu*
+┣⊱❥ *${prefix}elang*
+┣⊱❥ *${prefix}play*
+┣⊱❥ *${prefix}anjing*
+┣⊱❥ *${prefix}babi*
+┣⊱❥ *${prefix}playstore*
+┣⊱❥ *${prefix}url2image*
+┣⊱❥ *${prefix}kbbi*
+┣⊱❥ *${prefix}imoji*
+┣⊱❥ *${prefix}wait*
+┃
+┣━━━━°❀ ❬ *CREATOR* ❭ ❀°━━━━┓
+┃
+┣⊱❥ *${prefix}thunder*
+┣⊱❥ *${prefix}tahta*
+┣⊱❥ *${prefix}glitch <teks|teks>*
+┣⊱❥ *${prefix}phlogo <teks|teks>*
+┣⊱❥ *${prefix}wolflogo <teks|teks>*
+┣⊱❥ *${prefix}wolflogo2 <teks|teks>*
+┣⊱❥ *${prefix}quotemaker <tx|wtrmk|tema>*
+┣⊱❥ *${prefix}galaxtext*
+┣⊱❥ *${prefix}textdark*
+┣⊱❥ *${prefix}textblue*
+┣⊱❥ *${prefix}lovemake*
+┣⊱❥ *${prefix}stiltext*
+┣⊱❥ *${prefix}ninjalogo*
+┣⊱❥ *${prefix}party*
+┣⊱❥ *${prefix}rtext*
+┣⊱❥ *${prefix}water*
+┣⊱❥ *${prefix}lionlogo <teks|teks>*
+┣⊱❥ *${prefix}textscreen*
+┣⊱❥ *${prefix}text3d*
+┣⊱❥ *${prefix}epep*
+┣⊱❥ *${prefix}marvelogo <teks|teks>*
+┣⊱❥ *${prefix}snow <teks|teks>*
+┣⊱❥ *${prefix}firetext*
+┃
+┣━━━°❀ ❬ *FUN&GAMES* ❭ ❀°━━━┓
+┃
+┣⊱❥ *${prefix}truth*
+┣⊱❥ *${prefix}dare*
+┣⊱❥ *${prefix}tebakgambar*
+┣⊱❥ *${prefix}family100*
+┣⊱❥ *${prefix}caklontong*
+┣⊱❥ *${prefix}qrcode*
+┣⊱❥ *${prefix}primbonjodoh*
+┣⊱❥ *${prefix}ramaljadian*
+┣⊱❥ *${prefix}mlherolist*
+┣⊱❥ *${prefix}bucin*
+┣⊱❥ *${prefix}persengay*
+┣⊱❥ *${prefix}ramalhp <nomor>*
+┣⊱❥ *${prefix}ceckjodoh*
+┃
+┣━━━━━°❀ ❬ *ANIME* ❭ ❀°━━━━━┓
+┃
+┣⊱❥ *${prefix}openanime*
+┣⊱❥ *${prefix}Naruto*
+┣⊱❥ *${prefix}Minato*
+┣⊱❥ *${prefix}Boruto*
+┣⊱❥ *${prefix}Hinata*
+┣⊱❥ *${prefix}Sakura*
+┣⊱❥ *${prefix}Sasuke*
+┣⊱❥ *${prefix}kaneki*
+┣⊱❥ *${prefix}toukacan*
+┣⊱❥ *${prefix}rize*
+┣⊱❥ *${prefix}akira*
+┣⊱❥ *${prefix}itori*
+┣⊱❥ *${prefix}kurumi*
+┣⊱❥ *${prefix}miku*
+┣⊱❥ *${prefix}anime*
+┣⊱❥ *${prefix}nekonime*
+┣⊱❥ *${prefix}loli*
+┣⊱❥ *${prefix}loli2*
+┣⊱❥ *${prefix}waifu*
+┣⊱❥ *${prefix}waifu2*
+┣⊱❥ *${prefix}wibu*
+┣⊱❥ *${prefix}randomanime*
+┣⊱❥ *${prefix}pokemon*
+┣⊱❥ *${prefix}artinama*
+┃
+┣━━°❀ ❬ *INFO&EDUKASI* ❭ ❀°━━━┓
+┃
+┣⊱❥ *${prefix}infogc*
+┣⊱❥ *${prefix}infogempa*
+┣⊱❥ *${prefix}infogithub*
+┣⊱❥ *${prefix}infocuaca*
+┣⊱❥ *${prefix}infonomor*
+┣⊱❥ *${prefix}infomobil*
+┣⊱❥ *${prefix}infomotor*
+┣⊱❥ *${prefix}grupinfo*
+┣⊱❥ *${prefix}lirik*
+┣⊱❥ *${prefix}quotes*
+┣⊱❥ *${prefix}cerpen*
+┣⊱❥ *${prefix}chord*
+┣⊱❥ *${prefix}wiki*
+┣⊱❥ *${prefix}brainly*
+┣⊱❥ *${prefix}resepmasakan*
+┣⊱❥ *${prefix}map*
+┃
+┣━━━━━°❀ ❬ *GRUP* ❭ ❀°━━━━━━┓
+┃
+┣⊱❥ *${prefix}add*
+┣⊱❥ *${prefix}kick*
+┣⊱❥ *${prefix}promote*
+┣⊱❥ *${prefix}demote*
+┣⊱❥ *${prefix}setname*
+┣⊱❥ *${prefix}setdesc*
+┣⊱❥ *${prefix}welcome*
+┣⊱❥ *${prefix}nsfw*
+┣⊱❥ *${prefix}simih*
+┣⊱❥ *${prefix}grup [buka/tutup]*
+┣⊱❥ *${prefix}tagme*
+┣⊱❥ *${prefix}hidetag*
+┣⊱❥ *${prefix}tagall*
+┣⊱❥ *${prefix}otagall*
+┣⊱❥ *${prefix}fitnah*
+┣⊱❥ *${prefix}infogc*
+┣⊱❥ *${prefix}grupinfo*
+┣⊱❥ *${prefix}linkgrup*
+┣⊱❥ *${prefix}listadmins*
+┣⊱❥ *${prefix}openanime*
+┣⊱❥ *${prefix}edotense*
+┣⊱❥ *${prefix}kalkulator*
+┃
+┣━━━━━°❀ ❬ *NSFW* ❭ ❀°━━━━━━┓
+┃
+┣⊱❥ *${prefix}nsfwloli*
+┣⊱❥ *${prefix}nsfwblowjob*
+┣⊱❥ *${prefix}nsfwneko*
+┣⊱❥ *${prefix}nsfwtrap*
+┣⊱❥ *${prefix}randomhentai*
+┣⊱❥ *${prefix}hentai*
+┣⊱❥ *${prefix}indohot*
+┃
+┣━━━━°❀ ❬ *KERANG* ❭ ❀°━━━━━┓
+┃
+┣⊱❥ *${prefix}apakah*
+┣⊱❥ *${prefix}kapankah*
+┣⊱❥ *${prefix}bisakah*
+┣⊱❥ *${prefix}rate*
+┣⊱❥ *${prefix}watak*
+┣⊱❥ *${prefix}hobby*
+┃
+┣━━━━━°❀ ❬ *OTHER* ❭ ❀°━━━━━┓
+┃
+┣⊱❥ *${prefix}blocklist*
+┣⊱❥ *${prefix}testime*
+┣⊱❥ *${prefix}hilih*
+┣⊱❥ *${prefix}say*
+┣⊱❥ *${prefix}delete*
+┣⊱❥ *${prefix}shorturl*
+┃
+┣━━━━°❀ ❬ *OWNER* ❭ ❀°━━━━━┓
+┃
+┣⊱❥ *${prefix}bc*
+┣⊱❥ *${prefix}ban*
+┣⊱❥ *${prefix}block*
+┣⊱❥ *${prefix}unblock*
+┣⊱❥ *${prefix}clearall*
+┣⊱❥ *${prefix}clone*
+┣⊱❥ *${prefix}getses*
+┣⊱❥ *${prefix}setpp*
+┣⊱❥ *${prefix}setpp*
+┣⊱❥ *${prefix}leave*
+┃
+┣━━━━━°❀ ❬ *NAME* ❭ ❀°━━━━━┓
+┃
+┃          *GAP FINDER INDONESIA*
+┃
+┃
+┣━━━━°❀ *❬ SOSMED ❭* ❀°━━━━⊱
+┃
+┃ *Ada masalah? Hub :*
+┃ _http://wa.me/6281352445558_
+┃ *Instagram :* https://www.instagram.com/d_c_d_j
+┃ *YouTube :* https://m.youtube.com/channel/UCMVX_iwpCSrywG0EyHBW8gw
+┃ *Instagram GFI :* https://www.instagram.com/gapfinderindonesia/
+┃ *Telegram :* https://t.me/gapfinderindonesia
+┣━━━━°❀ *❬ NOTE ❭* ❀°━━━━━━⊱
+┃
+┃Bot ini belum selesai sepenuhnya
+┃Masih dalam proses pengerjaan
+┃Jadi masih jarang aktif.
+┃
+┣━━━━━°❀ ❬ *TQTO* ❭ ❀°━━━━━┛
+┃
+┃ *SPECIAL THANGKS TO🤝*
+┣⊱❥ *${prefix}DANDI SAPUTRA*
+┣⊱❥ *${prefix}./ROYCO*
+┣⊱❥ *${prefix}AFNI*
+┣⊱❥ *${prefix}V1T4*
+┣⊱❥ *${prefix}BRAM*
+┣⊱❥ *${prefix}YOGI*
+┣⊱❥ *${prefix}YUDHA*
+┣⊱❥ *${prefix}ALL MEM GFI*
+┣⊱❥ *${prefix}DLL*
+┃
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+┃ *❬ POWERED BY GAP FINDER INDONESIA ❭*
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`
+}
+exports.help = help
